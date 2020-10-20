4ee79c2315d6f132b2469ca257ca919f0a8662b0
Alireza.Sadeghi <alireza.sadeghi@tatasteeleurope.com>
2020-10-20 22:34:38 +0200

    #Method to list items in a given folder -> pd.Dataframe()

--- pysharepoint/interface.py
+++ pysharepoint/interface.py
@@ -12,10 +12,13 @@ class SPInterface:
         self.username = username
         self.password = password
         self.sharepoint_base_url = sharepoint_base_url
-        self.authcookie = Office365(sharepoint_base_url, username=username, password=password).GetCookies()
+        self.authcookie = Office365(
+            sharepoint_base_url, username=username, password=password
+        ).GetCookies()
 
-
-    def download_file_sharepoint(self, source_path, sink_path, filename, sharepoint_site):
+    def download_file_sharepoint(
+        self, source_path, sink_path, filename, sharepoint_site
+    ):
         """This fucntion will download a file from the Sharepoint to specified sink path.
         Parameters:
             source_path = r'Shared Documents/Shared/<Location>'
@@ -31,21 +34,24 @@ class SPInterface:
         folder = site.Folder(source_path)
         for attempt in range(0, 3):
             try:
-                output_file = open(full_sink_path, 'wb')
+                output_file = open(full_sink_path, "wb")
                 input_file = folder.get_file(filename)
                 binary_format = bytearray(input_file)
                 output_file.write(binary_format)
                 output_file.close()
                 print("Attempt #No: ", attempt)
-                print("Downlowded file size is ", round(os.path.getsize(full_sink_path) / 1024, 2), " KB")
+                print(
+                    "Downlowded file size is ",
+                    round(os.path.getsize(full_sink_path) / 1024, 2),
+                    " KB",
+                )
             except Exception as e:
-                if (attempt < 2):
+                if attempt < 2:
                     print("Try again!")
                     continue
                 print("Error", e)
                 raise e
             break
-
 
     def upload_file_sharepoint(self, source_path, sink_path, filename, sharepoint_site):
         """This fucntion will upload a file from the source path to Sharepoint.
@@ -61,24 +67,24 @@ class SPInterface:
         print(full_source_path)
         print(full_sink_path)
         folder = site.Folder(sink_path)
-        with open(full_source_path, mode='rb') as file:
+        with open(full_source_path, mode="rb") as file:
             filecontent = file.read()
         for attempt in range(0, 3):
             try:
-              folder.upload_file(filecontent, full_sink_path)
-              print ("Attempt #No:", attempt)
+                folder.upload_file(filecontent, full_sink_path)
+                print("Attempt #No:", attempt)
             except Exception as e:
-              if(attempt<2):
-                print("Trying again!")
-                continue
-              print("Error", e)
-              raise e
+                if attempt < 2:
+                    print("Trying again!")
+                    continue
+                print("Error", e)
+                raise e
             break
 
     def list_item_sharepoint(self, directory_path, sharepoint_site):
         site = Site(sharepoint_site, version=Version.v2016, authcookie=self.authcookie)
         folder_source = site.Folder(directory_path)
-        #get files in a directory
+        # get files in a directory
         files_item = folder_source.files
 
         items_df = pd.DataFrame()
@@ -87,26 +93,44 @@ class SPInterface:
 
         if len(items_df) > 0:
             # subset the columns
-            subset_cols = ['Length', 'LinkingUrl', 'MajorVersion','MinorVersion', 'Name', 'TimeCreated', 'TimeLastModified']
+            subset_cols = [
+                "Length",
+                "LinkingUrl",
+                "MajorVersion",
+                "MinorVersion",
+                "Name",
+                "TimeCreated",
+                "TimeLastModified",
+            ]
             items_df = items_df[subset_cols]
 
-            #parse url to remove everything after ? mark
-            items_df['LinkingUrl'] = [ i.split('?')[0] for i in items_df['LinkingUrl']]
+            # parse url to remove everything after ? mark
+            items_df["LinkingUrl"] = [i.split("?")[0] for i in items_df["LinkingUrl"]]
             # convert bytes to KB
-            items_df['Length'] = [round(int(i)/1000,2) for i in items_df['Length']]
-            #sort based on file names
-            items_df.sort_values('Name', inplace=True)
+            items_df["Length"] = [round(int(i) / 1000, 2) for i in items_df["Length"]]
+            # sort based on file names
+            items_df.sort_values("Name", inplace=True)
 
             # rename to more friendlier names
-            items_df.columns = ['FileSize', 'FullFileUrl', 'FileVersion','MinorVersion', 'FileName', 'TimeCreated', 'TimeLastModified']
+            items_df.columns = [
+                "FileSize",
+                "FullFileUrl",
+                "FileVersion",
+                "MinorVersion",
+                "FileName",
+                "TimeCreated",
+                "TimeLastModified",
+            ]
             return items_df
         else:
-            print(f'No files in {directory_path} directory')
+            print(f"No files in {directory_path} directory")
             return pd.DataFrame()
+
 
 def main():
     """Main function"""
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
